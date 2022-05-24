853be7ec985b427412450a163b1e0f00c11cf640
AISSAME BOUKHALLAD <59233636+aissameXyz@users.noreply.github.com>
2022-05-24 23:48:56 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\FiliereController;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
