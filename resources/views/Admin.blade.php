14d2aa544da4dd94dc85f995b131d80be95d2469
ashraf khabar <khabarachraf@gmail.com>
2022-05-25 18:10:47 +0100

    commit

--- resources/views/Admin.blade.php
+++ resources/views/Admin.blade.php
@@ -1,18 +1,16 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
+
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in as an Admin !
+
                 </div>
                 <div class="p-6 bg-white border-b border-gray-200">
-                <a href="{{url('students')}}"> All Students </a> 
+                <a href="{{url('students')}}"> All Students </a>
                 </div>
                                 <div class="p-6 bg-white border-b border-gray-200">
                                 <a href="{{url('teacher')}}">All Teachers</a>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,4 +1,41 @@
-<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
+<nav class="navbar navbar-light navbar-expand-md navigation-clean-button" style="background-color: var(--bs-gray-200)">
+    <div class="container"><a class="navbar-brand" href="{{ url('/dashboard') }}">Our logo</a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+        <div class="collapse navbar-collapse" id="navcol-1">
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">About</a></li>
+                <li class="nav-item dropdown"><a class="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown" href="#">Services</a>
+                    <div class="dropdown-menu"><a class="dropdown-item" href="#">Notes</a><a class="dropdown-item" href="#">Notes</a><a class="dropdown-item" href="#">Notes</a></div>
+                </li>
+            </ul>
+            <span class="navbar-text actions">
+                    @if (Route::has('login'))
+                        <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                            @auth
+                                <ul>
+                                    <form method="POST" action="{{ route('logout') }}">
+                                        @csrf
+                                        <a class="btn btn-light action-button" role="button" href="{{ url('/dashboard') }}" style="background: var(--bs-red);">Dashboard</a>
+                                        <button class="btn btn-light action-button" type="submit"  style="background: var(--bs-red);">Log out</button>
+
+                                    </form>
+                                </ul>
+
+                            @else
+                                <a class="btn btn-light action-button" role="button" href="{{ route('login') }}" style="background: var(--bs-red);">Log in</a>
+                                @if (Route::has('register'))
+                                    <a class="btn btn-light action-button" role="button" href="{{ route('register') }}" style="background: var(--bs-red);">Register</a>
+                                @endif
+                            @endauth
+                        </div>
+                    @endif
+                </span>
+            </div>
+    </div>
+</nav>
+
+
+{{--<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
@@ -39,7 +76,7 @@
                             @csrf
 
                             <x-dropdown-link :href="route('logout')"
-                                    onclick="event.preventDefault();
+                                             onclick="event.preventDefault();
                                                 this.closest('form').submit();">
                                 {{ __('Log Out') }}
                             </x-dropdown-link>
@@ -81,7 +118,7 @@
                     @csrf
 
                     <x-responsive-nav-link :href="route('logout')"
-                            onclick="event.preventDefault();
+                                           onclick="event.preventDefault();
                                         this.closest('form').submit();">
                         {{ __('Log Out') }}
                     </x-responsive-nav-link>
@@ -89,4 +126,42 @@
             </div>
         </div>
     </div>
-</nav>
+</nav>--}}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
